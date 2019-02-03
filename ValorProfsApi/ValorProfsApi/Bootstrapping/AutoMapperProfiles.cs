8b851990f2bb699be3bd0d4c91d984e356a2e06b
Jesus <jmgarnica94@gmail.com>
2019-02-03 17:46:40 -0200

    Add AutomapperProfile and add error to response extension

---
+++ ValorProfsApi/ValorProfsApi/Bootstrapping/AutoMapperProfiles.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using ValorProfsApi.Data.Entities;
+using ValorProfsApi.Dtos;
+using AutoMapper;
+using System;
+
+namespace ValorProfsApi.Bootstrapping
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<ProductToCreateDto, Product>();
+            CreateMap<ProductToUpdateDto, Product>();
+            CreateMap<Product, ProductToListDto>();
+        }
+    }
+}
